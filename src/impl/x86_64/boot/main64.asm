0b949e4f23651663cd34ebb5318a346646b13c40
VGenesis <91892895+VGenesis@users.noreply.github.com>
2022-08-07 17:44:22 +0200

    Basic kernel

--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -1,4 +1,6 @@
 global long_mode_start
+extern kernel_main
+
 section .text
 bits 64
 long_mode_start:
@@ -10,8 +12,8 @@ long_mode_start:
     mov fs, ax
     mov gs, ax
     
-    ; print 'OK'
-    mov dword [0xb8000], 0x2f4b2f4f
+    ; start c code
+    call kernel_main
     
     hlt
 
